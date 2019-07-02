c1da28447b5547615237d81c887eda5cbbd6e254
Mr.aum <52208551+pattanapong9811@users.noreply.github.com>
2019-07-02 21:31:31 +0700

    Add files via upload

---
+++ ppap.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void main()
+
+{
+
+float thbahts ; 
+
+
+cout << "Enter Baht : "  ; 
+cin >> thbahts ;
+
+
+cout <<"***Exchang Rate***"<< endl ;  
+cout <<thbahts <<" Bahts is "<< thbahts*0.033 << " Dollars"<< endl ;  
+cout <<thbahts <<" Bahts is "<< thbahts*0.029 << " Euro"<< endl ;
+
+cout <<" "<< endl ;
+
+cout <<" "<< endl ;
+}
